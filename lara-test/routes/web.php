85a2b9c350f694c6709a803ecf4c1f5a97fa7cc4
kentarou2023 <124994773+kentarou2023@users.noreply.github.com>
2023-03-08 13:34:30 +0800

    routes/web.php edit編集後

--- lara-test/routes/web.php
+++ lara-test/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get(‘/tasks/{id}/edit’ ,  ‘TaskController@edit’)->name(‘tasks.edit’);
+
+
