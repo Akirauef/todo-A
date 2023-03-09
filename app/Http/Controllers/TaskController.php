1ec741a1afe5b57be414dd6f8bd7c199d73437a5
Masato <saint.k.2525@gmail.com>
2023-03-10 01:03:53 +0800

    initial commit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 use App\Task;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 class TaskController extends Controller
 {
     function index()
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -12,9 +12,8 @@
                 </div>
                 <div class="form-group">
                     <label>内容</label>
-                    <textarea class="form-control" rows="5" name="body”>{{.$task->contents }}</textarea>
+                    <textarea class="form-control" rows="5" name="contents">{{ $task->body }}</textarea>
                 </div>
-                {{-- submitのダブルクォーテーションを変更しても字が赤い --}}
                 <button type="submit" class="btn btn-primary">更新する</button>
             </form>
         </div>
